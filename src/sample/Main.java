2c5420e0c7280ed7594be1ab27b7cda56843d5dd
SamraMujcinovic <ssmujcinovi1@etf.unsa.ba>
2018-12-25 12:38:49 +0100

    zavrsen main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,50 +1,35 @@
 package sample;
 
-import com.sun.security.ntlm.Client;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
-import java.io.InputStream;
-import java.net.Proxy;
-import java.net.Socket;
-import java.net.URL;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class Main extends Application {
-    public static Stage m;
-
+    public static Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         FileListModel model = new FileListModel();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/finder.fxml"));
-        loader.setController(new FinderController(model));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        loader.setController(new Controller(model));
         Parent root = loader.load();
         primaryStage.setTitle("Pretraga datoteka");
-        m=primaryStage;
+        stage = primaryStage;
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.show();
-        primaryStage.setOnHiding(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                System.exit(0);
-            }
+
+        primaryStage.setOnHiding((event) -> {
+            System.exit(0);
         });
-
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
